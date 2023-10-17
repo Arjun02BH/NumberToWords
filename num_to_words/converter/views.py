1196e9cb4e99e466d41edcc03bb56fd9bf451708
Arjun02BH <121733134+Arjun02BH@users.noreply.github.com>
2023-10-17 10:25:02 +0530

    Created views for the converter app

--- num_to_words/converter/views.py
+++ num_to_words/converter/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+from num2words import num2words  # You may need to install this package with pip
+
+def number_to_words(request):
+    if request.method == 'POST':
+        number = request.POST['number']
+        words = num2words(number)
+        return render(request, 'converter/number_to_words.html', {'number': number, 'words': words})
+    return render(request, 'converter/number_to_words.html')
